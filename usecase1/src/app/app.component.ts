2f9f4a31f691286d75d7c26f00a1820702f33488
3n3a <info@3n3a.ch>
2022-06-09 11:17:33 +0200

    add cusotm welcome page

--- usecase1/src/app/app.component.ts
+++ usecase1/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Trips', url: '/trips', icon: 'airplane' },
     { title: 'About', url: '/about', icon: 'information-circle' },
   ];
-  constructor() {}
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+  }
+
+  isWelcomePage() {  
+    return this.router.url === "/welcome"
+  }
 }
