adddbc11b313430f337ddf7174bc03a56b6e52af
3n3a <info@3n3a.ch>
2022-06-15 13:22:36 +0200

    add environemt.ts

--- exam/src/environments/environment.ts
+++ exam/src/environments/environment.ts
@@ -1,6 +1,18 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    // Hier einfügen
+    apiKey: "AIzaSyAGfKXsiZDLvxPw_IdJVj6kkguumXRG-zc",
+
+    authDomain: "m335-projekt.firebaseapp.com",
+
+    databaseURL: "https://m335-projekt.firebaseio.com",
+
+    projectId: "m335-projekt",
+
+    storageBucket: "m335-projekt.appspot.com",
+
+    messagingSenderId: "661342522206",
+
+    appId: "1:661342522206:web:a119a60b993bbdea28ddcf"
   }
 };
